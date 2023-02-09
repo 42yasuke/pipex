7682749f96f029000eddf40894a4834124a2673e
yasuke <j.ralph1998@gmail.com>
2023-02-09 16:59:50 +0100

    il manque le bonus

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: jose <jose@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/28 01:15:55 by jose              #+#    #+#             */
-/*   Updated: 2023/02/09 15:35:13 by jose             ###   ########.fr       */
+/*   Updated: 2023/02/09 16:36:47 by jose             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,9 @@ int	main(int ac, char **av, char **envp)
 	if (ac != 5)
 		ft_error(BAD_PARAMETERS, NULL);
 	fd1 = open(av[1], O_RDONLY, 0444);
-	fd2 = open(av[ac - 1], O_CREAT | O_WRONLY | O_TRUNC, 0644);
+	fd2 = open(av[ac - 1], O_CREAT | O_WRONLY | O_TRUNC | O_EXCL, 0644);
+	if (fd2 == -1)
+		fd2 = open(av[ac - 1], O_CREAT | O_WRONLY | O_TRUNC, 0244);
 	if (fd1 == -1)
 		ft_error(FILE_CANT_BE_READ, av[1]);
 	if (fd2 == -1)
